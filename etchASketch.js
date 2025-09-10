7a918c936eb961c60f4675472475aaab418db032
FhuSoe <rnurmarianto@yahoo.com>
2025-09-10 14:50:58 +0700

    Added the function to add the grids and the loop in the function

--- etchASketch.js
+++ etchASketch.js
@@ -1,1 +1,17 @@
-const container = document.querySelector("#container")
+const container = document.getElementById("grid-container");
+const gridSize = 16;
+
+function createGrid(size) {
+    //to clear any existing cells
+    container.innerHTML = "";
+
+    //The loop to create the grid based on the size
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        container.appendChild(cell);
+    }
+}
+
+//calling the function the to create grids when the page loads
+createGrid(gridSize);
